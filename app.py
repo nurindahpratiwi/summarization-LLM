b2c5e07087ebdd9e22ea698fa0e7b5444ef87885
nurindahpratiwi <nurindahpratiwi71@gmail.com>
2023-10-30 20:12:01 +0700

    fix model

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.chains.summarize import load_summarize_chain
 from transformers import T5Tokenizer
 from transformers import pipeline
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+
 import torch
 import base64
 
 #MODEL AND TOKENIZER
 checkpoint = "MBZUAI/LaMini-Flan-T5-248M"
-tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
+#tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+#base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+base_model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
 #FILE LOADER AND PREPROCESSING
 def get_file(file):
-    loader = PyPDFloader(file)
+    loader = PyPDFLoader(file)
     pages = loader.load_and_split()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
     texts = text_splitter.split_documents(pages)
