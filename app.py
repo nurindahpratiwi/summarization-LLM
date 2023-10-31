aa61dd5eb4821a7bb5ab052bf817beaec6d31419
nurindahpratiwi <nurindahpratiwi71@gmail.com>
2023-10-31 08:37:02 +0700

    fix model

--- app.py
+++ app.py
@@ -1,61 +1,51 @@
-import streamlit as st
+import streamlit as st 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import PyPDFLoader
+
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from transformers import pipeline
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-
-
 import torch
 import base64
 
-#MODEL AND TOKENIZER
-checkpoint = "MBZUAI/LaMini-Flan-T5-248M"
-tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
-#tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-#base_model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+# Model and tokenizer 
+model_checkpoint = "LaMini-Flan-T5-248M"
+model_tokenizer = T5Tokenizer.from_pretrained(model_checkpoint)
+model = T5ForConditionalGeneration.from_pretrained(model_checkpoint, device_map='auto', torch_dtype=torch.float32)
 
-#FILE LOADER AND PREPROCESSING
-def get_file(file):
-    loader = PyPDFLoader(file)
+# File loader and preprocessing
+def preprocess_pdf(file):
+    loader =  PyPDFLoader(file)
     pages = loader.load_and_split()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=170, chunk_overlap=70)
     texts = text_splitter.split_documents(pages)
-    final_texts = ""
+    final_text = ""
     for text in texts:
-        print (text)
-        final_texts = final_texts + text.page_content
-    return final_texts
+        final_text = final_text + text.page_content
+    return final_text
 
-def llm_pipeline(filepath):
-    pipe_sum = pipeline(
+# Language Model pipeline
+def language_model_pipeline(filepath):
+    summarization_pipeline = pipeline(
         'summarization',
-        model = base_model,
-        tokenizer = tokenizer,
-        max_length = 500,
-        min_length = 50
-    )
-    input_text = get_file(filepath)
-    result = pipe_sum(input_text)
-    result = result[0]['summary_text']
-    return result
+        model=model,
+        tokenizer=model_tokenizer,
+        max_length=500, 
+        min_length=70)
+    input_text = preprocess_pdf(filepath)
+    summary_result = summarization_pipeline(input_text)
+    summarized_text = summary_result[0]['summary_text']
+    return summarized_text
 
 @st.cache_data
-#FUNCTION TO DISPLAY PDF
-def displayPDF(file):
-    #Opening file
-    with open(file, 'rb') as f:
+# Function to display the PDF content
+def display_pdf(file):
+    with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    
-    pdf_display = F'<iframe src="data:application/pdf;base64, {base64_pdf}" width = "100%" height="600" type="application/pdf"></iframe>'
 
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-#streamlit code
+# Streamlit code 
 st.set_page_config(layout="wide")
 
 def main():
@@ -66,17 +56,17 @@ def main():
     if uploaded_file is not None:
         if st.button("Summarize"):
             col1, col2 = st.columns(2)
-            filepath = "data/"+ uploaded_file.name
-            with open (filepath, "wb") as temp_file:
-                temp_file.write (uploaded_file.read())
+            filepath = "pdf/" + uploaded_file.name
+            with open(filepath, "wb") as temp_file:
+                temp_file.write(uploaded_file.read())
             with col1:
-                st.info("Uploaded file")
-                pdf_viewer = displayPDF(filepath)
-            with col2:
-                summary = llm_pipeline(filepath)
-                st.info("Summarization Complete")
-                st.success(summary)
+                st.info("Uploaded File")
+                pdf_view = display_pdf(filepath)
 
+            with col2:
+                summarized_result = language_model_pipeline(filepath)
+                st.info("Summarization Complete")
+                st.success(summarized_result)
 
 if __name__ == "__main__":
     main()
